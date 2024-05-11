07b14601aa3758deb523e68c0402584440b6172a
Kian Kasad <kian@kasad.com>
2024-05-11 16:25:49 -0700

    Add flat_map iterator

--- src/map.c
+++ src/map.c
@@ -1,4 +1,5 @@
 #include "map.h"
+#include "iterator.h"
 
 #include <stdlib.h>
 
@@ -19,7 +20,7 @@ static void citer_map_free_data(void *_data) {
     free(data);
 }
 
-iterator_t *citer_map(iterator_t *orig, void *(*fn)(void *)) {
+iterator_t *citer_map(iterator_t *orig, citer_map_fn_t fn) {
     citer_map_data_t *data = malloc(sizeof(*data));
     *data = (citer_map_data_t) { .orig = orig, .fn = fn };
     iterator_t *it = malloc(sizeof(*it));
@@ -27,6 +28,57 @@ iterator_t *citer_map(iterator_t *orig, void *(*fn)(void *)) {
         .data = data,
         .next = citer_map_next,
         .free_data = citer_map_free_data
+    };
+    return it;
+}
+
+typedef struct citer_flat_map_data {
+    iterator_t *orig;
+    iterator_t *(*fn)(void *);
+    iterator_t *cur;
+} citer_flat_map_data_t;
+
+static void *citer_flat_map_next(void *_data) {
+    citer_flat_map_data_t *data = (citer_flat_map_data_t *) _data;
+    for (;;) {
+        if (data->cur) {
+            void *item = citer_next(data->cur);
+            if (item) {
+                return item;
+            } else {
+                citer_free(data->cur);
+                data->cur = NULL;
+            }
+        }
+        void *orig_item = citer_next(data->orig);
+        if (!orig_item) {
+            return NULL;
+        }
+        data->cur = data->fn(orig_item);
+    }
+}
+
+static void citer_flat_map_free_data(void *_data) {
+    citer_flat_map_data_t *data = (citer_flat_map_data_t *) _data;
+    citer_free(data->orig);
+    if (data->cur) {
+        citer_free(data->cur);
+    }
+    free(data);
+}
+
+iterator_t *citer_flat_map(iterator_t *orig, citer_flat_map_fn_t fn) {
+    citer_flat_map_data_t *data = malloc(sizeof(*data));
+    *data = (citer_flat_map_data_t) {
+        .orig = orig,
+        .fn = fn,
+        .cur = NULL
+    };
+    iterator_t *it = malloc(sizeof(*it));
+    *it = (iterator_t) {
+        .data = data,
+        .next = citer_flat_map_next,
+        .free_data = citer_flat_map_free_data
     };
     return it;
 }
--- src/map.h
+++ src/map.h
@@ -5,6 +5,10 @@
 
 #include "iterator.h"
 
-iterator_t *citer_map(iterator_t *, void *(*)(void *));
+typedef void *(*citer_map_fn_t)(void *);
+typedef iterator_t *(*citer_flat_map_fn_t)(void *);
+
+iterator_t *citer_map(iterator_t *, citer_map_fn_t);
+iterator_t *citer_flat_map(iterator_t *, citer_flat_map_fn_t);
 
 #endif /* _CITER_MAP_H_ */
